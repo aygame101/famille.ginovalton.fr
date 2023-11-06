85cd8d33d20f738d8efecea10bc4d6e5a3d74f4a
aygame101 <aygame101@gmail.com>
2023-11-06 20:57:52 +0100

    first req

--- req-creation.sql
+++ req-creation.sql
@@ -1,1 +1,9 @@
--- Requete pour creer la premiere table
+-- Requete pour creer la table "user"
+CREATE TABLE fm_gv_fr.user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    identifiant VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    date_creation DATETIME NOT NULL,
+    activated TINYINT(1) NOT NULL
+);
