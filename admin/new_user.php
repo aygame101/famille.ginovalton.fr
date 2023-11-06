81017bd0c63cd91de136348e1e9f77919a90961c
aygame101 <aygame101@gmail.com>
2023-11-06 21:31:04 +0100

    Create and Connect working

--- admin/new_user.php
+++ admin/new_user.php
@@ -15,10 +15,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
 
     // Génération d'un sel aléatoire
-    $salt = bin2hex(random_bytes(16)); // Longueur recommandée : au moins 16 octets (128 bits)
+    $salt = bin2hex(random_bytes(4)); // Longueur recommandée : au moins 16 octets (128 bits)
+
 
     // Hashage sécurisé du mot de passe avec bcrypt
-    $hashedPassword = password_hash($password . $salt, PASSWORD_BCRYPT);
+    $hashedPassword = hash('sha256', $password . $salt);
 
     // Date de création actuelle
     $creationDate = date('Y-m-d H:i:s');
