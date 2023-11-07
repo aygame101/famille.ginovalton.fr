d14718e075c6d8c90734a2350cc82a97df977621
aygame101 <aygame101@gmail.com>
2023-11-07 19:41:24 +0100

    ça marche pas, mais y a l'idée, je vais manger

--- process_php/up_picture.php
+++ process_php/up_picture.php
@@ -1,5 +1,14 @@
 <?php
 
+require_once('europe.php');
+
+try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_username, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+
 // Fonction pour ajouter un suffixe unique de 8 caractères aléatoires au nom de fichier original
 function generateNewFilename($originalFileName) {
     // Séparer le nom de fichier de son extension
@@ -29,7 +38,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['photo'])) {
         $dest_path = $uploadFileDir . $newFileName;
 
         if(move_uploaded_file($fileTmpPath, $dest_path)) {
-            echo 'File is successfully uploaded.';
+            $message = $_POST['message'] ?? '';
+            $user_id = $_SESSION['user_id'] ?? '0'; // Remplacez '0' par une gestion d'erreur appropriée
+
+            // Insertion des informations dans la base de données
+            $sql = "INSERT INTO photos (chemin, message, user) VALUES (:chemin, :message, :user)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([
+                ':chemin' => $dest_path,
+                ':message' => $message,
+                ':user' => $user_id
+            ]);
+
+            echo 'File is successfully uploaded and data has been saved to the database.';
         } else {
             echo 'There was an error moving the file to upload directory. Please make sure the upload directory is writable.';
         }
--- req-creation.sql
+++ req-creation.sql
@@ -7,3 +7,12 @@ CREATE TABLE fm_gv_fr.users (
     date_creation DATETIME NOT NULL,
     activated TINYINT(1) NOT NULL
 );
+
+
+CREATE TABLE fm_gv_fr.photos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    chemin VARCHAR(255) NOT NULL,
+    message TEXT,
+    user INT,
+    FOREIGN KEY (user) REFERENCES users(id)  -- Supposons qu'il y a une table 'users' avec une colonne 'id'
+); -- ENGINE=InnoDB DEFAULT CHARSET=utf8;
