9f1b14421a0f91851aaeb7789ad4980d6aaa84f5
aygame101 <aygame101@gmail.com>
2023-11-10 10:58:23 +0100

    ok on revient en arrière, pour linstant ça marche

--- process_php/up_picture.php
+++ process_php/up_picture.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once('europe.php');
+session_start();
 
-try {
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_username, $db_password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
+if (!isset($_SESSION['user_id'])) {
+    // Si l'utilisateur n'est pas connecté, redirigez-le vers la page de connexion
+    header("Location: connexion.php");
+    exit;
 }
 
 // Fonction pour ajouter un suffixe unique de 8 caractères aléatoires au nom de fichier original
@@ -29,28 +28,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['photo'])) {
     $fileName = $_FILES['photo']['name'];
     $allowedfileExtensions = array('png', 'jpeg', 'jpg', 'heic');
 
-    // Obtenez l'extension du fichier // A partir d'ici ça fonctionne pas
+    // Obtenez l'extension du fichier
     $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
 
     if (in_array($fileExtension, $allowedfileExtensions)) {
         $newFileName = generateNewFilename($fileName);
-        $uploadFileDir = '../img_import/'; // PENSER dossier accessible en écriture
+        $uploadFileDir = '../img_import/'; // Assurez-vous que ce dossier existe et est accessible en écriture
         $dest_path = $uploadFileDir . $newFileName;
 
         if(move_uploaded_file($fileTmpPath, $dest_path)) {
-            $message = $_POST['message'] ?? '';
-            $user_id = $_SESSION['user_id'] ?? '0'; // Remplacez '0' par une gestion d'erreur appropriée
-
-            // Insertion des informations dans la base de données
-            $sql = "INSERT INTO photos (chemin, message, user) VALUES (:chemin, :message, :user)";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([
-                ':chemin' => $dest_path,
-                ':message' => $message,
-                ':user' => $user_id
-            ]);
-               
-            echo 'File is successfully uploaded and data has been saved to the database.';
+            echo 'File is successfully uploaded.';
+            $user_ied = $_SESSION['user_id'];
+            echo "$user_ied";
         } else {
             echo 'There was an error moving the file to upload directory. Please make sure the upload directory is writable.';
         }
