ffe5e54d534dd30bb2906e463dd9851861096603
aygame101 <aygame101@gmail.com>
2023-11-13 19:44:32 +0100

    le css cest parti on commence

--- process_php/connexion.php
+++ process_php/connexion.php
@@ -48,6 +48,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="css/styles_index.css">
+
     <title>Connexion</title>
 </head>
 <body>
