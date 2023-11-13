b04f17ec771f62d71406d58fc532d09c4512913d
aygame101 <aygame101@gmail.com>
2023-11-13 19:47:00 +0100

    correct path to css file

--- process_php/connexion.php
+++ process_php/connexion.php
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="css/styles_index_conn.css">
+    <link type="text/css" rel="stylesheet" href="../css/styles_index_conn.css">
 
     <title>Connexion</title>
 </head>
